29b41b0ab4f19bd87103bb0c02118b50f3bb5c78
yenifer <lizz_gonzalez@hotmail.com>
2015-03-20 18:33:35 -0600

    ultimoRepositorio

---
+++ AlejandraEjercicios/DIRECCION.php
@@ -0,0 +1,18 @@
+<?php
+$comentario;
+$calle;
+$colonia;
+$casa;
+$municipio;
+$departamento;
+
+{
+print $comentario = 'Mi direccion es:';
+print $calle= 'Calle Francisco Morazan';
+print $colonia= ' Colonia Alvarado,';
+print $casa= 'Casa n° 20';
+print $municipio= 'Municipio Santiago Nonualco,';
+print $departamento= ' Departamento la paz,';
+
+}
+?>
